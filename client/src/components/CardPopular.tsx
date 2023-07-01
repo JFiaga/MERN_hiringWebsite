be5be53362db26ed01446d06fa037cf45b0b7a62
Fiaga Fils <fiagafils@gmail.com>
2023-07-01 23:29:26 +0100

    Add the carousel in MainPopular section

--- client/src/components/CardPopular.tsx
+++ client/src/components/CardPopular.tsx
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
-   title:string;
-   desc:string;
-   img:string;
+  title: string;
+  desc: string;
+  link: string;
+  img: string;
+};
 
-}
-
-const CardPopular = ({title, desc, img}:Props) => {
-
+const CardPopular = ({ title, desc, img, link }: Props) => {
   return (
-    <div
+    <div className="bg-black cursor-pointer h-[345px] w-[90%] sm:max-w-[300px] relative overflow-hidden rounded-sm">
+        <Link to={link}>
+        <img
+          src={img}
+          alt=""
+          className="h-full w-full hover:scale-125 object-cover object-right z-20 transition-all duration-500 opacity-50"
+        />
 
-    className="bg-black cursor-pointer h-[345px] w-[90%] sm:max-w-[300px] relative overflow-hidden rounded-sm"
-  >
-    <img
-      src={img}
-      alt=""
-      className="h-full w-full hover:scale-125 object-cover object-right z-20 transition-all duration-500 opacity-50"
-    />
+        <div className="flex flex-col absolute top-[50%] -translate-y-[50%]">
+          <span className=" md:text-lg mx-4 mt-2">{title}</span>
+          <span className="font-bold capitalize text-lg md:text-2xl mx-4">
+            {desc}
+          </span>
+        </div>
+    </Link>
+      </div>
+  );
+};
 
-    <div className="flex flex-col absolute top-[50%] -translate-y-[50%]">
-      <span className=" md:text-lg mx-4 mt-2">{title}</span>
-      <span className="font-bold capitalize text-lg md:text-2xl mx-4">
-        {desc}
-      </span>
-    </div>
-  </div>
-  )
-}
-
-export default CardPopular
+export default CardPopular;
--- client/src/containers/Main/MainPopular.tsx
+++ client/src/containers/Main/MainPopular.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Saxo, laravel } from "../../assets/index";
 import { CardPopular } from "../../components";
+import Slider from "infinite-react-carousel";
+
 
 // type Props = {};
 
@@ -26,21 +28,47 @@ const MainPopular = () => {
       desc: "PHP - Laravel",
       img: laravel,
     },
+    {
+      title: "Backend developper ",
+      desc: "PHP - Symphony",
+      img: Saxo,
+    },
+    {
+      title: "Backend developper ",
+      desc: "PHP - Laravel",
+      img: laravel,
+    },
   ];
 
   return (
-    <section className=" flex flex-col w-full max-w-[1400px] md:px-8 px-4 pt-16 pb-10 ">
+    <section className=" flex  w-[100vw]  md:px-8 px-4 pt-16 pb-10 text-white justify-center ">
+      
+      <div className="max-w-[1400px] w-full flex flex-col items-center">
+
+        
       <h3 className="text-black text-2xl font-semibold mb-12 text-center">
         Popular services
       </h3>
 
-      <Link to="/gigs?category=backend">
-        <div className="flex space-x-2 items-center">
-          {cardDesc.map((card, index) => (
-            <CardPopular title={card.title} desc={card.desc} img={card.img} />
-          ))}
-        </div>
-      </Link>
+          
+      <Slider
+      
+      slidesToShow={5} arrowScroll={5} className={`carousel relative w-full max-w-[1400px]`}>
+        
+        {cardDesc.map((card, index) => (
+       
+            <CardPopular
+            key={index}
+              title={card.title}
+              desc={card.desc}
+              link={"/gigs?category=backend"}
+              img={card.img}
+            />
+          
+        ))}
+      </Slider>
+      </div>
+
     </section>
   );
 };
