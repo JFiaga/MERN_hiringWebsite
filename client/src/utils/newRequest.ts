c82d726fa99212a1b51075c9ef2901151d06f3b2
Fiaga Fils <fiagafils@gmail.com>
2023-07-22 12:46:29 +0100

    add the images compressed

--- client/src/utils/newRequest.ts
+++ client/src/utils/newRequest.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// export const newRequest = axios.create({
+//     baseURL:"http://localhost:8800/api/",
+//     withCredentials: true
+// });
+
+
 export const newRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"https://jhire-api.vercel.app/api",
     withCredentials: true
 });
 
