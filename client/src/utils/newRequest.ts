1dbe98fe1916c41a796c0d07f92766336d35f506
Fiaga Fils <fiagafils@gmail.com>
2023-07-26 16:33:14 +0100

    update the local api to the online api

--- client/src/utils/newRequest.ts
+++ client/src/utils/newRequest.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"https://jhireapi.onrender.com/api/",
     withCredentials: true
 });
 
-
-// export const newRequest = axios.create({
-//     baseURL:"https://jhire-api.vercel.app/api",
-//     withCredentials: true
-// });
 
